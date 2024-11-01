5b33ace3bd0321f7f08ffbeead73d5df9c995c90
Yuskur <yisakor.melke@gmail.com>
2024-10-31 23:35:31 -0400

    .

--- server/server.js
+++ server/server.js
@@ -2,13 +2,38 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const jwt = require('jsonwebtoken');
+const crypto = require(crypto);
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://goldenMonkey:<30jKEVDJsBvneiFs>@soloprojectgs.txfzh.mongodb.net/?retryWrites=true&w=majority&appName=SoloProjectGS";
 
 const app = express();
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json()); //parse incoming json payloads in http requests
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
 
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+
+  
 
 const port = 3001;
 
@@ -24,6 +49,8 @@ app.post('/login', (req, res) => {
 });
 
 app.signup('');
+
+run().catch(console.dir); // Make connection to api
 
 app.listen(port, () => {
     console.log("listening");
